f625b4cb54f74283d3e556d7e1ed91e211febd9b
alicee03 <alicee03@naver.com>
2026-05-06 14:34:01 +0900

    feat: GitHub 로그인/회원가입 콜백 페이지 추가 #1

---
+++ src/app/github/callback/page.tsx
@@ -0,0 +1,49 @@
+'use client'
+
+import { useEffect } from 'react'
+import { useRouter, useSearchParams } from 'next/navigation'
+
+const REDIRECT_URI = `${process.env.NEXT_PUBLIC_FRONTEND_URL}/auth/github/callback`
+
+async function githubLogin(code: string) {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/auth/github`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ code, redirectUri: REDIRECT_URI }),
+  })
+  return res.json()
+}
+
+export default function GithubCallbackPage() {
+  const router = useRouter()
+  const searchParams = useSearchParams()
+
+  useEffect(() => {
+    const code = searchParams.get('code')
+    if (!code) {
+      router.push('/')
+      return
+    }
+
+    githubLogin(code).then((res) => {
+      if (res.success) {
+        if (res.data.isNewUser) {
+          sessionStorage.setItem('tempUserId', res.data.tempUserId)
+          router.push('/signup/github')
+        } else {
+          localStorage.setItem('accessToken', res.data.accessToken)
+          router.push('/')
+        }
+      } else {
+        alert('GitHub 로그인에 실패했습니다!')
+        router.push('/login')
+      }
+    })
+  }, [])
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <p className="body-m">로그인 중...</p>
+    </div>
+  )
+}
