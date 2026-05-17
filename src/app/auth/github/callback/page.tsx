5e610158fd99633a34cdae93e8978c6ca40ea2e8
alicee03 <alicee03@naver.com>
2026-05-17 21:39:20 +0900

    fix: 로그인 후 이동 경로 /home으로 수정

--- src/app/auth/github/callback/page.tsx
+++ src/app/auth/github/callback/page.tsx
@@ -13,7 +13,7 @@ function GithubCallbackContent() {
   useEffect(() => {
     const code = searchParams.get('code')
     if (!code) {
-      router.push('/')
+      router.push('/home')
       return
     }
 
@@ -25,7 +25,7 @@ function GithubCallbackContent() {
         } else {
           sessionStorage.removeItem('tempUserId')
           localStorage.setItem('accessToken', res.data.accessToken)
-          router.push('/')
+          router.push('/home')
         }
       } else {
         alert('GitHub 로그인에 실패했습니다!')
